398379e2769a1d65fa3fd51fe432c582f965b4b7
MonishJN <jnmonish@gmail.com>
2026-05-19 00:14:35 +0530

    first commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -49,7 +49,7 @@ public class GameManager : MonoBehaviour
         // 3. Load the new level
         previousScene = currentScene;
         currentLevel++;
-        if (currentLevel == 9)
+        if (currentLevel == 11)
         {
             //Debug.Log("Work in Progress! Just Play from the Start!");
             //currentLevel = 1;
