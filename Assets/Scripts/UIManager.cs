dfcdec8ef075ff42c385bbea042a21d344c4923f
MonishJN <jnmonish@gmail.com>
2026-05-25 00:54:45 +0530

    first commit

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,8 +2,30 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private GameObject tutorialUI;
-    public void Learnt() { 
+   [SerializeField] private GameObject tutorialUI;
+private int currentChild = 1;
+
+public void Learnt()
+{
+    // If we've reached the last child, hide the whole tutorial UI
+    if (currentChild >= tutorialUI.transform.childCount - 1)
+    {
+        // Disable the current child
+        // tutorialUI.transform.GetChild(currentChild).gameObject.SetActive(false);
+
+        // Finally disable the whole tutorial panel
         tutorialUI.SetActive(false);
     }
+    else
+    {
+        // Disable the current child
+        tutorialUI.transform.GetChild(currentChild).gameObject.SetActive(false);
+
+        // Move to the next child
+        currentChild++;
+
+        // Enable the next child
+        tutorialUI.transform.GetChild(currentChild).gameObject.SetActive(true);
+    }
+}
 }
